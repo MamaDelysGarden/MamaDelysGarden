aea47939b597eca27f8659f87e490d7a4043c7b7
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-07-19 14:07:39 +0800

    REM

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,9 @@
 div(v-if="events")
     .window-height.row(style="padding-top: 64px")
         .m-a.text-center.text-itc
-            div(style="font-size: 200px").text-primary Mama Dely's
-            div(style="font-size: 200px").text-green Garden
-            div(style="font-size: 100px").text-primay cafe,resto &amp; function hall
+            div(style="font-size: 2rem").text-primary Mama Dely's
+            div(style="font-size: 2rem").text-green Garden
+            div(style="font-size: 2rem").text-primay cafe,resto &amp; function hall
     q-carousel.text-white(arrows dots infinite style="height: calc(50vh - 50px)", @slide="setSlide")
         template(v-for="(event,i) in events")
             .relative-position.fit(slot="slide").no-padding
